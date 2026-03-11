37ce5aec864337721b1b7c95e6d33db17fda8264
sleepingWolf44 <44sleepingwolf@gmail.com>
2026-03-11 09:03:42 -0500

    Add files via upload

---
+++ Back End/test.py
@@ -0,0 +1,71 @@
+import psutil
+import time
+
+psutil.cpu_percent(interval=None) #starts the call then waits so it works the next time
+time.sleep(1)
+
+# CPU
+
+cpu_percent = psutil.cpu_percent(interval=1)
+
+cpu_per_core = psutil.cpu_percent(interval=1, percpu=True)
+
+cpu_freq = psutil.cpu_freq()._asdict()
+
+# RAM
+
+ram_used = (psutil.virtual_memory().used)/1073741824
+
+ram_total = (psutil.virtual_memory().total)/1073741824
+
+ram_percent = psutil.virtual_memory().percent
+
+swap_percent = psutil.swap_memory().percent
+
+# Disk
+disk_data = {}
+
+for part in psutil.disk_partitions():
+    usage = psutil.disk_usage(part.mountpoint)
+
+disk_total = (usage.total)/1073741824
+
+disk_used = (usage.used)/1073741824
+
+disk_percent = usage.percent
+
+read_bytes = (psutil.disk_io_counters().read_bytes)/1073741824
+
+write_bytes = (psutil.disk_io_counters().write_bytes)/1073741824
+
+# Thermal
+cpu_temp = None
+system_temp = None
+try:
+    temps = psutil.sensors_temperatures()
+    if not temps:
+        print("Temperature sensors not supported on this system or no data available.")
+    else:
+        cpu_temp = temps.coretemp()
+
+        system_temp = temps
+
+except AttributeError:
+    print("psutil.sensors_temperatures() is not supported on this OS.")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+print(f"CPU percent: {cpu_percent}%")
+print(f"CPU core percent: {cpu_per_core}%")
+print(f"CPU freq: {cpu_freq}")
+print(f"ram_used: {ram_used:.2f} GB")
+print(f"ram_total: {ram_total:.2f} GB")
+print(f"ram_percent: {ram_percent}%")
+print(f"swap_percent: {swap_percent}%")
+print(f"disk_total: {disk_total:.2f} GB")
+print(f"disk_used: {disk_used:.2f} GB")
+print(f"disk_percent: {disk_percent}%")
+print(f"read_bytes: {read_bytes:.2f} GB")
+print(f"write_bytes: {write_bytes:.2f} GB")
+print(f"cpu_temp: {cpu_temp}%")
+print(f"system_temp: {system_temp}%")
