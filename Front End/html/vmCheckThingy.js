ef6135545f5b904af12771fa992dba75259dc25b
Kataclysmix <dawnbringer@versiontwenty.com>
2026-04-14 10:57:28 -0500

    debugging vm check; run dev won't work in FE1

--- Front End/html/vmCheckThingy.js
+++ Front End/html/vmCheckThingy.js
@@ -1,138 +1,18 @@
 // Random stuff that is needed
-let x = 0
+let nodeStatus = 0;
 
+const element = document.getElementsByClassName("vmObject");
 // Goes below or above 0 if it has an error and resets back to 0 when the error is fixed.
 function notWorky(x) {
-    const element = document.getElementsByClassName("vmObject");
-    if (x == 0) {
-        element.style.color = "#FFFFFF";
+    if (x != 0) {
+        element.style.color = "#00FF00";
     } else {
-        element.style.color = "#ff0000";
+        element.style.class = "critical"
     }
 }
 
-function notWorkyOne2() {
-    const element = document.getElementById("one2");
-    if (notWorkyOne2 == 0) {
-        element.style.color = "#FFFFFF";
-    } else {
-        element.style.color = "#ff0000";
-    }
-}
-
-function notWorkyOne3() {
-    const element = document.getElementById("one3");
-    if (notWorkyOne3 == 0) {
-        element.style.color = "#FFFFFF";
-    } else {
-        element.style.color = "#ff0000";
-    }
-}
-
-function notWorkyOne4() {
-    const element = document.getElementById("one4");
-    if (notWorkyOne4 == 0) {
-        element.style.color = "#FFFFFF";
-    } else {
-        element.style.color = "#ff0000";
-    }
-}
-
-function notWorkyOne5() {
-    const element = document.getElementById("one5");
-    if (notWorkyOne5 == 0) {
-        element.style.color = "#FFFFFF";
-    } else {
-        element.style.color = "#ff0000";
-    }
-}
-
-function notWorkyTwo1() {
-    const element = document.getElementById("two1");
-    if (notWorkyTwo1 == 0) {
-        element.style.color = "#FFFFFF";
-    } else {
-        element.style.color = "#ff0000";
-    }
-}
-
-function notWorkyTwo2() {
-    const element = document.getElementById("two2");
-    if (notWorkyTwo2 == 0) {
-        element.style.color = "#FFFFFF";
-    } else {
-        element.style.color = "#ff0000";
-    }
-}
-
-function notWorkyTwo3() {
-    const element = document.getElementById("two3");
-    if (notWorkyTwo3 == 0) {
-        element.style.color = "#FFFFFF";
-    } else {
-        element.style.color = "#ff0000";
-    }
-}
-
-function notWorkyTwo4() {
-    const element = document.getElementById("two4");
-    if (notWorkyTwo4 == 0) {
-        element.style.color = "#FFFFFF";
-    } else {
-        element.style.color = "#ff0000";
-    }
-}
-
-function notWorkyTwo5() {
-    const element = document.getElementById("two5");
-    if (notWorkyTwo5 == 0) {
-        element.style.color = "#FFFFFF";
-    } else {
-        element.style.color = "#ff0000";
-    }
-}
-
-function notWorkyThree1() {
-    const element = document.getElementById("three1");
-    if (notWorkyThree1 == 0) {
-        element.style.color = "#FFFFFF";
-    } else {
-        element.style.color = "#ff0000";
-    }
-}
-
-function notWorkyThree2() {
-    const element = document.getElementById("three2");
-    if (notWorkyThree2 == 0) {
-        element.style.color = "#FFFFFF";
-    } else {
-        element.style.color = "#ff0000";
-    }
-}
-
-function notWorkyThree3() {
-    const element = document.getElementById("three3");
-    if (notWorkyThree3 == 0) {
-        element.style.color = "#FFFFFF";
-    } else {
-        element.style.color = "#ff0000";
-    }
-}
-
-function notWorkyThree4() {
-    const element = document.getElementById("three4");
-    if (notWorkyThree4 == 0) {
-        element.style.color = "#FFFFFF";
-    } else {
-        element.style.color = "#ff0000";
-    }
-}
-
-function notWorkyThree5() {
-    const element = document.getElementById("three5");
-    if (notWorkyThree5 == 0) {
-        element.style.color = "#FFFFFF";
-    } else {
-        element.style.color = "#ff0000";
-    }
+let i = 0;
+for(i in element){
+    notWorky(nodeStatus);
+    i++;
 }
