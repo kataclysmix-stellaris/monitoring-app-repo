028278c72e75fb327bac5e9324bc1ccf3c568a92
Elainaisyes <elijah.chesser@students.cvtech.edu>
2026-04-28 12:28:40 -0500

    Fixed URLS

--- APIs/hello/urls.py
+++ APIs/hello/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from hello import SendTelemetry, GetTelemetry
+from hello import SendTelemetry, GetTelemetry, cookie_auth
 
 urlpatterns = [
     path("", GetTelemetry.home, name="home"),
     path("api/sendtelemetry", SendTelemetry.home),
     path("api/gettelemetry/", GetTelemetry.home),
     path("api/gettelemetry/<str:table_id>/", GetTelemetry.search),
-    path("api/login/", LoginView.as_view(), name="login"),
-    path("api/refresh/", RefreshCookieView.as_view(), name="token_refresh"),
-    path("api/logout/", LogoutView.as_view(), name="logout"),
+    path("api/login/", cookie_auth.LoginView.as_view(), name="login"),
+    path("api/refresh/", cookie_auth.RefreshCookieView.as_view(), name="token_refresh"),
+    path("api/logout/", cookie_auth.LogoutView.as_view(), name="logout"),
 ]
